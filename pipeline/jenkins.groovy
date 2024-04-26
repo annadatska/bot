aa3329800a2b3549351ca6cad1f6928ae2ec9088
Anna Datska <79701344+annadatska@users.noreply.github.com>
2024-04-26 20:12:25 +0000

    upd jenkins file

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,80 +1,61 @@
 pipeline {
     agent any
-    environment{
-        REPO = 'https://github.com/annadatska/bot'
-        BRANCH = 'develop'
-        DOCKER='datskadevops'
-    }
     parameters {
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-        choice(name: 'ARCH', choices: ['amd64', 'arch64'], description: 'Pick Arch')
-
+        choice(name: 'OS', choices: ['linux', 'apple', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
+
+    environment {
+        GITHUB_TOKEN=credentials('annadatska')
+        REPO = 'https://github.com/annadatska/bot.git'
+        BRANCH = 'main'
+    }
+
     stages {
+
         stage('clone') {
             steps {
-                echo "Clone repo"
+                echo 'Clone Repository'
                 git branch: "${BRANCH}", url: "${REPO}"
             }
         }
-        stage('test'){
-            steps{
-                echo "Test Build"
-                sh 'make test'
+
+        stage('test') {
+            steps {
+                echo 'Testing started'
+                sh "make test"
             }
         }
-        stage('build'){
-            steps{
-                script{
-                    echo "Start build application"
-                    if (params.OS == "linux" && params.ARCH == "amd64"){
-                        sh 'make linux'
-                    }
-                    else if (params.OS == "linux" && params.ARCH == "arm64"){
-                        sh 'make linux_arm'
-                    }
-                    else if (params.OS == "windows" && params.ARCH == "amd64"){
-                        sh 'make windows'
-                    }
-                    else if (params.OS == "windows" && params.ARCH == "arm64"){
-                        echo "Sorry, ARM arch not supported for windows"
-                    }
-                    else if (params.OS == "macos"){
-                        echo "Sorry, MacOS not supported"
-                    }
-                }
+
+        stage('build') {
+            steps {
+                echo "Building binary for platform ${params.OS} on ${params.ARCH} started"
+                sh "make ${params.OS} ${params.ARCH}"
             }
         }
-        stage('image'){
-            steps{
-                script{
-                    echo "Start build docker image"
-                    if (params.OS == "linux" && params.ARCH == "amd64"){
-                        sh 'make image_linux'
-                    }
-                    else if (params.OS == "linux" && params.ARCH == "arm64"){
-                        sh 'make image_linux_arm'
-                    }
-                    else if (params.OS == "windows" && params.ARCH == "amd64"){
-                        sh 'make image_windows'
-                    }
-                    else if (params.OS == "windows" && params.ARCH == "arm64"){
-                        echo "Sorry, ARM arch not supported for windows"
-                    }
-                    else if (params.OS == "macos"){
-                        echo "Sorry, MacOS not supported"
-                    }
-                }
+
+        stage('image') {
+            steps {
+                echo "Building image for platform ${params.OS} on ${params.ARCH} started"
+                sh "make image-${params.OS} ${params.ARCH}"
             }
         }
-        stage('push'){
-            steps{
-                script{
-                    docker.withRegistry('', 'dockerhub'){
-                        sh 'make push'
-                    }
-                }
+        
+        stage('login to GHCR') {
+            steps {
+                sh "echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_TOKEN_USR --password-stdin"
             }
+        }
+
+        stage('push image') {
+            steps {
+                sh "make -n ${params.OS} ${params.ARCH} image push"
+            }
+        } 
+    }
+    post {
+        always {
+            sh 'docker logout'
         }
     }
 }
